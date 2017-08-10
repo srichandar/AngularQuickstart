8ab71bead0423d53cb91b573e27d9cd56dab5ca6
srichandar <srichandar@gmail.com>
2017-08-10 18:05:15 +0530

    initial commit

---
+++ src/app/components/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  template: `<h1>About Initialized...</h1>
+    
+  `  
+})
+export class AboutComponent  {
+
+
+}
+ 
---
+++ src/app/components/user.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { PostService } from '../services/post.services';
+
+@Component({
+  moduleId: module.id,
+  selector: 'user',
+  templateUrl: 'user.component.html',
+  providers: [PostService]
+  
+})
+export class UserComponent  { 
+name: string;
+hobbies: string[];
+posts: Post
+constructor(private postService: PostService) {
+	this.name = "john doe";
+	this.hobbies = ["movies", "music", "sports"];
+	this.postService.getPosts().subscribe(posts => {
+	   this.posts = posts;
+	});
+}
+
+addHobby(hobby) {
+	this.hobbies.push(hobby.value);
+	hobby.value = '';
+}
+
+deleteHobby(index) {
+	this.hobbies.splice(index, 1);
+}
+
+
+ }
+ 
+ interface Post {
+	 id: number;
+	 title: string;
+	 body: string;
+ }
